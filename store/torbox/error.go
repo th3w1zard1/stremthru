c5c0fb33c4f3b2a6148d02c6572e4fdc80d66cc0
Munif Tanjim <hello@muniftanjim.dev>
2024-11-06 12:22:53 +0600

    refactor(store/torbox): move error code

--- store/torbox/error.go
+++ store/torbox/error.go
@@ -7,6 +7,41 @@ import (
 	"github.com/MunifTanjim/stremthru/store"
 )
 
+type ErrorCode string
+
+const (
+	ErrorCodeDatabaseError           ErrorCode = "DATABASE_ERROR"
+	ErrorCodeUnknownError            ErrorCode = "UNKNOWN_ERROR"
+	ErrorCodeNoAuth                  ErrorCode = "NO_AUTH"
+	ErrorCodeBadToken                ErrorCode = "BAD_TOKEN"
+	ErrorCodeAuthError               ErrorCode = "AUTH_ERROR"
+	ErrorCodeInvalidOption           ErrorCode = "INVALID_OPTION"
+	ErrorCodeRedirectError           ErrorCode = "REDIRECT_ERROR"
+	ErrorCodeOAuthVerificationError  ErrorCode = "OAUTH_VERIFICATION_ERROR"
+	ErrorCodeEndpointNotFound        ErrorCode = "ENDPOINT_NOT_FOUND"
+	ErrorCodeItemNotFound            ErrorCode = "ITEM_NOT_FOUND"
+	ErrorCodePlanRestrictedFeature   ErrorCode = "PLAN_RESTRICTED_FEATURE"
+	ErrorCodeDuplicateItem           ErrorCode = "DUPLICATE_ITEM"
+	ErrorCodeBozoRssFeed             ErrorCode = "BOZO_RSS_FEED"
+	ErrorCodeSellixError             ErrorCode = "SELLIX_ERROR"
+	ErrorCodeTooMuchData             ErrorCode = "TOO_MUCH_DATA"
+	ErrorCodeDownloadTooLarge        ErrorCode = "DOWNLOAD_TOO_LARGE"
+	ErrorCodeMissingRequiredOption   ErrorCode = "MISSING_REQUIRED_OPTION"
+	ErrorCodeTooManyOptions          ErrorCode = "TOO_MANY_OPTIONS"
+	ErrorCodeBozoTorrent             ErrorCode = "BOZO_TORRENT"
+	ErrorCodeNoServersAvailableError ErrorCode = "NO_SERVERS_AVAILABLE_ERROR"
+	ErrorCodeMonthlyLimit            ErrorCode = "MONTHLY_LIMIT"
+	ErrorCodeCooldownLimit           ErrorCode = "COOLDOWN_LIMIT"
+	ErrorCodeActiveLimit             ErrorCode = "ACTIVE_LIMIT"
+	ErrorCodeDownloadServerError     ErrorCode = "DOWNLOAD_SERVER_ERROR"
+	ErrorCodeBozoNzb                 ErrorCode = "BOZO_NZB"
+	ErrorCodeSearchError             ErrorCode = "SEARCH_ERROR"
+	ErrorCodeInvalidDevice           ErrorCode = "INVALID_DEVICE"
+	ErrorCodeDiffIssue               ErrorCode = "DIFF_ISSUE"
+	ErrorCodeLinkOffline             ErrorCode = "LINK_OFFLINE"
+	ErrorCodeVendorDisabled          ErrorCode = "VENDOR_DISABLED"
+)
+
 func UpstreamErrorWithCause(cause error) *core.UpstreamError {
 	err := core.NewUpstreamError("")
 	err.StoreName = string(store.StoreNameTorBox)
--- store/torbox/response.go
+++ store/torbox/response.go
@@ -16,41 +16,6 @@ const (
 	ResponseStatusError   ResponseStatus = "error"
 )
 
-type ErrorCode string
-
-const (
-	ErrorCodeDatabaseError           ErrorCode = "DATABASE_ERROR"
-	ErrorCodeUnknownError            ErrorCode = "UNKNOWN_ERROR"
-	ErrorCodeNoAuth                  ErrorCode = "NO_AUTH"
-	ErrorCodeBadToken                ErrorCode = "BAD_TOKEN"
-	ErrorCodeAuthError               ErrorCode = "AUTH_ERROR"
-	ErrorCodeInvalidOption           ErrorCode = "INVALID_OPTION"
-	ErrorCodeRedirectError           ErrorCode = "REDIRECT_ERROR"
-	ErrorCodeOAuthVerificationError  ErrorCode = "OAUTH_VERIFICATION_ERROR"
-	ErrorCodeEndpointNotFound        ErrorCode = "ENDPOINT_NOT_FOUND"
-	ErrorCodeItemNotFound            ErrorCode = "ITEM_NOT_FOUND"
-	ErrorCodePlanRestrictedFeature   ErrorCode = "PLAN_RESTRICTED_FEATURE"
-	ErrorCodeDuplicateItem           ErrorCode = "DUPLICATE_ITEM"
-	ErrorCodeBozoRssFeed             ErrorCode = "BOZO_RSS_FEED"
-	ErrorCodeSellixError             ErrorCode = "SELLIX_ERROR"
-	ErrorCodeTooMuchData             ErrorCode = "TOO_MUCH_DATA"
-	ErrorCodeDownloadTooLarge        ErrorCode = "DOWNLOAD_TOO_LARGE"
-	ErrorCodeMissingRequiredOption   ErrorCode = "MISSING_REQUIRED_OPTION"
-	ErrorCodeTooManyOptions          ErrorCode = "TOO_MANY_OPTIONS"
-	ErrorCodeBozoTorrent             ErrorCode = "BOZO_TORRENT"
-	ErrorCodeNoServersAvailableError ErrorCode = "NO_SERVERS_AVAILABLE_ERROR"
-	ErrorCodeMonthlyLimit            ErrorCode = "MONTHLY_LIMIT"
-	ErrorCodeCooldownLimit           ErrorCode = "COOLDOWN_LIMIT"
-	ErrorCodeActiveLimit             ErrorCode = "ACTIVE_LIMIT"
-	ErrorCodeDownloadServerError     ErrorCode = "DOWNLOAD_SERVER_ERROR"
-	ErrorCodeBozoNzb                 ErrorCode = "BOZO_NZB"
-	ErrorCodeSearchError             ErrorCode = "SEARCH_ERROR"
-	ErrorCodeInvalidDevice           ErrorCode = "INVALID_DEVICE"
-	ErrorCodeDiffIssue               ErrorCode = "DIFF_ISSUE"
-	ErrorCodeLinkOffline             ErrorCode = "LINK_OFFLINE"
-	ErrorCodeVendorDisabled          ErrorCode = "VENDOR_DISABLED"
-)
-
 type ResponseError struct {
 	Detail string    `json:"detail"`
 	Err    ErrorCode `json:"error"`
