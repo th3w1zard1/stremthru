d5a2c821b783a0b6dc76401b84c05c8b396ab253
Munif Tanjim <hello@muniftanjim.dev>
2024-12-07 13:15:25 +0600

    chore: install atlas and accept flags in scripts/schema.sh

--- scripts/schema.sh
+++ scripts/schema.sh
@@ -2,11 +2,32 @@
 
 set -euo pipefail
 
+echo_err() {
+  >&2 echo "$@"
+}
+
 declare -r cmd="${1:-"help"}"
 shift || true
 
-declare db_url="${STREMTHRU_DATABASE_URI}"
+declare atlas="atlas"
 
+if ! type $atlas >/dev/null 2>&1; then
+  atlas="${TMPDIR}/atlas"
+fi
+if ! type $atlas >/dev/null 2>&1; then
+  echo_err "Trying to download Atlas..."
+  curl -sSf https://atlasgo.sh | sh -s -- --community --no-install --output "${atlas}" --yes
+  chmod u+x "${atlas}"
+fi
+if ! type $atlas >/dev/null 2>&1; then
+  echo_err "Could not resolve: atlas"
+  exit 1
+fi
+
+echo_err "$($atlas version)"
+echo_err
+
+declare db_url="${STREMTHRU_DATABASE_URI}"
 declare schema_file="schema.hcl"
 
 case "${db_url}" in
@@ -17,10 +38,14 @@ esac
 
 case "${cmd}" in
 inspect)
-  atlas schema inspect -u "${db_url}"
+  echo_err \$ atlas schema inspect -u "\${STREMTHRU_DATABASE_URI}" "${@}"
+  echo_err
+  $atlas schema inspect -u "${db_url}" "${@}"
   ;;
 migrate)
-  atlas schema apply -u "${db_url}" --file "${schema_file}"
+  echo_err \$ schema apply -u "\${STREMTHRU_DATABASE_URI}" --file "${schema_file}" "${@}"
+  echo_err
+  $atlas schema apply -u "${db_url}" --file "${schema_file}" "${@}"
   ;;
 help)
   cat <<EOF
