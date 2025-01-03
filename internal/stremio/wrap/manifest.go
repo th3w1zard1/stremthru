6b9040b121a4e1ab5a8ddfbc14fb6a61a47a9327
Munif Tanjim <hello@muniftanjim.dev>
2025-01-03 23:29:12 +0600

    feat(stremio/wrap): add store hint in addon name

--- internal/stremio/wrap/manifest.go
+++ internal/stremio/wrap/manifest.go
@@ -1,13 +1,22 @@
 package stremio_wrap
 
 import (
+	"strings"
+
+	"github.com/MunifTanjim/stremthru/store"
 	"github.com/MunifTanjim/stremthru/stremio"
 )
 
 func getManifest(upstream *stremio.Manifest, ud *UserData) *stremio.Manifest {
 	manifest := upstream
 	manifest.ID = "st:wrap:" + manifest.ID
-	manifest.Name = "StremThru(" + manifest.Name + ")"
+	storeHint := ""
+	if ud.StoreName == "" {
+		storeHint = "ST|"
+	} else {
+		storeHint = strings.ToUpper(string(store.StoreName(ud.StoreName).Code())) + "|"
+	}
+	manifest.Name = "StremThru(" + storeHint + manifest.Name + ")"
 	manifest.BehaviorHints = &stremio.BehaviorHints{
 		Configurable:          true,
 		ConfigurationRequired: !ud.HasRequiredValues(),
