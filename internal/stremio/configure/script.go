2ffd31d9de70bc50cafde8313d6e12494c8fe43c
Munif Tanjim <hello@muniftanjim.dev>
2025-01-05 20:44:13 +0600

    feat(stremio): add link for easydebrid api key

--- internal/stremio/configure/script.go
+++ internal/stremio/configure/script.go
@@ -9,7 +9,7 @@ func GetScriptStoreTokenDescription(storeNameId, storeTokenId string) template.J
     "": "StremThru Basic Auth Token (base64 encoded) from <a href='https://github.com/MunifTanjim/stremthru?tab=readme-ov-file#configuration' target='_blank'><code>STREMTHRU_PROXY_AUTH</code></a>",
 		alldebrid: "AllDebrid <a href='https://alldebrid.com/apikeys' target='_blank'>API Key</a>",
 		debridlink: "DebridLink <a href='https://debrid-link.com/webapp/apikey' target='_blank'>API Key</a>",
-		easydebrid: "EasyDebrid API Key",
+		easydebrid: "EasyDebrid <a href='https://paradise-cloud.com/guides/easydebrid-api-key' target='_blank'>API Key</a>",
 		offcloud: "Offcloud <a href='https://offcloud.com/#/account' target='_blank'>credential</a> in <code>email:password</code> format, e.g. <code>john.doe@example.com:secret-password</code>",
 		premiumize: "Premiumize <a href='https://www.premiumize.me/account' target='_blank'>API Key</a>",
 		realdebrid: "RealDebrid <a href='https://real-debrid.com/apitoken' target='_blank'>API Token</a>",
