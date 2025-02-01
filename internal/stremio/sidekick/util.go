3d09426b77b6bebf5364cd37c9f9d6d01c08f18b
Munif Tanjim <hello@muniftanjim.dev>
2025-02-02 00:52:41 +0600

    fix(stremio/sidekick): do not escape html in json for backup

--- internal/stremio/sidekick/util.go
+++ internal/stremio/sidekick/util.go
@@ -17,7 +17,9 @@ func SendResponse(w http.ResponseWriter, statusCode int, data any) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
-	if err := json.NewEncoder(w).Encode(data); err != nil {
+	encoder := json.NewEncoder(w)
+	encoder.SetEscapeHTML(false)
+	if err := encoder.Encode(data); err != nil {
 		log.Printf("failed to encode json %v\n", err)
 	}
 }
